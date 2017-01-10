0ded4f6a85cbba2d574c97596409e5ee0c68da02
Ben Gazzard <bgazzard@palantir.com>
2017-01-10 16:36:36 -0500

    Add Alaric's solutions
    - His second solution doesn't seem to work, but I think I'm misreading
      it

---
+++ src/main/java/com/dartoxia/words/Dictionary.java
@@ -0,0 +1,39 @@
+package com.dartoxia.words;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+/**
+ * Created by bgazzard on 10/01/2017.
+ */
+public class Dictionary {
+
+    public static final List<char[]> words = ImmutableList.<char[]>builder()
+            .add("alpha".toCharArray())
+            .add("beta".toCharArray())
+            .add("gamma".toCharArray())
+            .add("delta".toCharArray())
+            .add("epsilon".toCharArray())
+            .add("zeta".toCharArray())
+            .add("eta".toCharArray())
+            .add("theta".toCharArray())
+            .add("iota".toCharArray())
+            .add("kappa".toCharArray())
+            .add("lambda".toCharArray())
+            .add("mu".toCharArray())
+            .add("nu".toCharArray())
+            .add("xi".toCharArray())
+            .add("omicron".toCharArray())
+            .add("pi".toCharArray())
+            .add("rho".toCharArray())
+            .add("sigma".toCharArray())
+            .add("tau".toCharArray())
+            .add("upsilon".toCharArray())
+            .add("phi".toCharArray())
+            .add("chi".toCharArray())
+            .add("psi".toCharArray())
+            .add("omega".toCharArray())
+            .build();
+
+}
--- src/test/java/com/dartoxia/words/TestWordSquare.java
+++ src/test/java/com/dartoxia/words/TestWordSquare.java
@@ -3,6 +3,8 @@ package com.dartoxia.words;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -26,5 +28,51 @@ public class TestWordSquare {
         assertTrue(ws.isComplete(Lists.newArrayList("abc".toCharArray(), "hed".toCharArray())));
         assertTrue(ws.isComplete(Lists.newArrayList("abcfihg".toCharArray())));
         assertFalse(ws.isComplete(Lists.newArrayList("efh".toCharArray())));
+    }
+
+    @Test
+    public void testAlaricSolutionValid() {
+        WordSquare ws = new WordSquare(9, 7);
+        ws.setGrid(new char[][] {
+                "azetled".toCharArray(),
+                "mthaumk".toCharArray(),
+                "gilonpa".toCharArray(),
+                "aspuapl".toCharArray(),
+                "mmercha".toCharArray(),
+                "panoimm".toCharArray(),
+                "ixihpob".toCharArray(),
+                "etocmed".toCharArray(),
+                "barhoga".toCharArray()
+        });
+
+        ws.print();
+
+        assertTrue(ws.isComplete(Dictionary.words));
+
+        List<char[]> invalidDictionary = Lists.newArrayList(Dictionary.words);
+        invalidDictionary.add("thiswordismissing".toCharArray());
+        assertFalse(ws.isComplete(invalidDictionary));
+    }
+
+    @Test
+    public void testAlaric2ndSolutionValid() {
+        WordSquare ws = new WordSquare(7, 8);
+        ws.setGrid(new char[][] {
+                "ppammbda".toCharArray(),
+                "aikyanus".toCharArray(),
+                "epsilome".toCharArray(),
+                "aukxphig".toCharArray(),
+                "thactrca".toCharArray(),
+                "zetoionl".toCharArray(),
+                "*bledahp".toCharArray()
+        });
+
+        ws.print();
+
+        assertTrue(ws.isComplete(Dictionary.words));
+
+        List<char[]> invalidDictionary = Lists.newArrayList(Dictionary.words);
+        invalidDictionary.add("thiswordismissing".toCharArray());
+        assertFalse(ws.isComplete(invalidDictionary));
     }
 }
